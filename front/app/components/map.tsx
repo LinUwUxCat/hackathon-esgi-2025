0971715bc711f27bfa6ee370f78df26d865908b7
LinuxCat <masch77.linuxcat@gmail.com>
2025-05-05 17:43:22 +0200

    Ajout geojson + données aléatoires

--- front/app/components/map.tsx
+++ front/app/components/map.tsx
@@ -1,27 +1,23 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { GeoJSON, MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import  HeatmapLayer from "react-leaflet-heat-layer"
+import getAisneGeoJSON from "~/services/aisne-geography";
+import { getDoctorLatLongs } from "~/services/doctor-lat-long";
 
 export default function DoctorMap() {
+  const [positions, setPositions] = useState<Number[][]>([]);
+  useEffect(() => {
+    getDoctorLatLongs().then(pos => setPositions(pos));
+  }, []);
+  
   
   return (
-    <MapContainer center={[49.4904, 3.7587]} zoom={10} scrollWheelZoom={true} style={{ height : "95vh", width: "100vw"}}>
+    <MapContainer center={[49.4904, 3.5587]} zoom={10} scrollWheelZoom={true} style={{ height : "95vh", width: "100vw"}}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <HeatmapLayer latlngs={[
-        [49.4904, 3.7587, 5.0],
-        [49.4904, 3.7597, 1.0],
-        [49.4904, 3.7687, 1.0],
-        [49.4904, 3.7787, 1.0],
-        [49.4904, 3.7887, 1.0],
-        [49.4904, 3.7987, 1.0],
-        [49.4904, 3.9587, 1.0]
-      ]} minOpacity={0.5} />
-      <Marker position={[49.4904, 3.7587]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <HeatmapLayer latlngs={positions} minOpacity={0.3} />
+      <GeoJSON data={getAisneGeoJSON()} style={() => {return {fill:false}}}/>
     </MapContainer>
 
   );
