bbb495e7aad30252862d949663c2ecbf50ba5e64
LinuxCat <masch77.linuxcat@gmail.com>
2025-05-05 12:31:17 +0200

    Ajout de la base

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    // Pour ajouter une route : 
+    // route("route_name", "routes/my_route.tsx")
+] satisfies RouteConfig;
