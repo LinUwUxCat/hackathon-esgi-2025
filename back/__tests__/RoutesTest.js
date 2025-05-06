252101a758df84a29f3fe951bfb4abacecc86aeb
LinuxCat <masch77.linuxcat@gmail.com>
2025-05-06 15:01:47 +0200

    Fix tests

--- back/__tests__/RoutesTest.js
+++ back/__tests__/RoutesTest.js
@@ -3,14 +3,14 @@
 test("Test Medecine Route", async () => {
     const response = await fetch("http://localhost:3050/medecins");
     expect(response.status).toBe(200);
-});
+}, 20000);
 
 test("Test Demo Route", async () => {
     const response = await fetch("http://localhost:3050/demo");
     expect(response.status).toBe(200);
-});
+}, 20000);
 
 test("Test Heatmap Route", async () => {
-    const response = await fetch("http://localhost:3050/heatmap");
+    const response = await fetch("http://localhost:3050/heatmap", {method:"POST"});
     expect(response.status).toBe(200);
-});
+}, 20000);
