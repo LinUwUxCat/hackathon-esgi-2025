3837f42186bebd4bf3fd6a2908f3251f26899afa
AppSketch <contact@appsketch.fr>
2025-05-06 09:29:03 +0200

    Add insee_id param to send specific demo insee

--- back/demo/demo.js
+++ back/demo/demo.js
@@ -7,4 +7,12 @@ router.get('/', (req, res) => {
     res.status(200).json(getAllDemo());
 });
 
+router.get('/:insee_id', (req, res) => {
+    const id = req.query.insee_id;
+    const demo = getAllDemo();
+    const demobyid = demo[id];
+    res.status(200);
+    res.send(demobyid);
+});
+
 module.exports = router;
